d00c87317fe92e72b406de0af8a085e0902da3f1
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-18 11:39:05 -0300

    exercicios

--- exercicio1.py
+++ exercicio1.py
@@ -1,1 +1,6 @@
 '''Escreva uma função que receba uma string e retorne o número de caracteres nela'''
+def conta_caracteres(frase):
+    return len(frase)
+
+frase = input('Digite a frase: ')
+print(conta_caracteres(frase))
--- exercicio2.py
+++ exercicio2.py
@@ -1,2 +1,7 @@
 '''Crie uma função que receba uma string e retorne a mesma string com todas as letras em 
 maiúsculas.'''
+def converte_maiusculo(frase):
+    return frase.upper()
+
+frase = input('Digite a frase: ')
+print(converte_maiusculo(frase))
