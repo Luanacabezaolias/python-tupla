dfc618f4cccda8e5e29515211a0a96655070db6b
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-22 09:48:34 -0300

    exercicios

--- exercicio7.py
+++ exercicio7.py
@@ -3,10 +3,21 @@ a) o maior número da lista
 b) o menor número da lista
 c) a média dos números contidos na lista'''
 
-lista = []
-for i in range(10):
-    num = input('Insira um número: ')
-    lista.append(num)
-print(max(lista))
-print(min(lista))
-print(sum(lista)/ len(lista))
+def preenche_lista():
+    lista = []
+    for i in range(10):
+        num = int(input('Insira um número: '))
+        lista.append(num)
+    print(lista)
+
+    maior = max(lista)
+    menor = min(lista)
+    media = sum(lista) / len(lista)
+
+    return maior, menor, media
+
+resultado = preenche_lista()
+print(resultado)
+print(f'Maior: {resultado[0]}')
+print(f'Menor: {resultado[1]}')
+print(f'Media: {resultado[2]}')
